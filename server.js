1723023677425157197a02479e4192d994157317
Teguh muhammad harits <areris981@gmail.com>
2020-11-24 20:21:53 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//panggil routes
+const routes = require('./router');
+routes(app)
+
 app.listen(8080, () => {
     console.log(`Server start on port 8080`);
 });
