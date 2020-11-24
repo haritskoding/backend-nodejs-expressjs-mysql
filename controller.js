7d07017964d91622b60485049e2bece102b16d48
Teguh muhammad harits <areris981@gmail.com>
2020-11-24 20:34:04 +0700

    Berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ const connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi REST Api ku berjalan",res)
 }
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+    connection.query("SELECT*FROM mahasiswa", function (err, rows, fields) {
+        if (err) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
